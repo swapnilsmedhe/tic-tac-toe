fc90b65e6c4b519c6fa47e7dadb2fd6b03ba444a
Swapnil Medhe <53039172+swapnilsmedhe@users.noreply.github.com>
2022-05-18 12:27:05 +0530

    Added tests for playRound

---
+++ test/testTicTacToe.js
@@ -0,0 +1,84 @@
+const assert = require('assert');
+const { playRound } = require('../src/ticTacToe.js');
+
+describe('playRound', () => {
+  it('should play a roud for player 1', () => {
+    return assert.deepStrictEqual(playRound({
+      player1: [],
+      player2: [],
+      currentPlayer: 'player1',
+      totalMoves: 0,
+      isGameOver: false
+    }, 1), {
+      player1: [1],
+      player2: [],
+      currentPlayer: 'player2',
+      totalMoves: 1,
+      isGameOver: false
+    });
+  });
+
+  it('should play a round for player 2', () => {
+    return assert.deepStrictEqual(playRound({
+      player1: [1],
+      player2: [],
+      currentPlayer: 'player2',
+      totalMoves: 1,
+      isGameOver: false
+    }, 5), {
+      player1: [1],
+      player2: [5],
+      currentPlayer: 'player1',
+      totalMoves: 2,
+      isGameOver: false
+    });
+  });
+
+  it('should play a round when the played move is winning move', () => {
+    return assert.deepStrictEqual(playRound({
+      player1: [5, 3],
+      player2: [1, 2],
+      currentPlayer: 'player1',
+      totalMoves: 4,
+      isGameOver: false
+    }, 7), {
+      player1: [5, 3, 7],
+      player2: [1, 2],
+      currentPlayer: 'player1',
+      totalMoves: 5,
+      isGameOver: true
+    });
+  });
+
+  it('should play a round when played move is last move of the game', () => {
+    return assert.deepStrictEqual(playRound({
+      player1: [5, 1, 6, 2],
+      player2: [3, 9, 8, 4],
+      currentPlayer: 'player1',
+      totalMoves: 8,
+      isGameOver: false
+    }, 7), {
+      player1: [5, 1, 6, 2, 7],
+      player2: [3, 9, 8, 4],
+      currentPlayer: 'player1',
+      totalMoves: 9,
+      isGameOver: true
+    });
+  });
+
+  it('should not play a round when played move is already played', () => {
+    return assert.deepStrictEqual(playRound({
+      player1: [4, 7],
+      player2: [5, 3],
+      currentPlayer: 'player1',
+      totalMoves: 4,
+      isGameOver: false
+    }, 3), {
+      player1: [4, 7],
+      player2: [5, 3],
+      currentPlayer: 'player1',
+      totalMoves: 4,
+      isGameOver: false
+    });
+  });
+});
